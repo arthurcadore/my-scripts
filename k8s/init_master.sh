93add4601f6a26015208c6053fecdba4e20dca0f
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-03-11 17:48:30 +0000

    :sparkles: updated master script

--- k8s/init_master.sh
+++ k8s/init_master.sh
@@ -7,3 +7,7 @@ echo "Initializing the kubeadm..."
 
 # create a new cluster, and redirect all the logs to the file init_master.log
 kubeadm init --v=5 --control-plane-endpoint=vm0.k8s.sj.ifsc.edu.br --pod-network-cidr=10.0.0.0/16 --service-cidr=10.1.0.0/16  > init_master.log 2>&1
+
+
+echo "Copying containerd configuration file to /etc/containerd/config.toml..."
+cat containerd-config.toml | sudo tee /etc/containerd/config.toml
